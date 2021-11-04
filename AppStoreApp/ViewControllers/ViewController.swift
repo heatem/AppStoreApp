f2000cc51fe2b0a79128ed8774193371ce831ea2
Heather Mason <heatem@gmail.com>
2021-11-04 16:41:09 -0400

    update background color to fix visibility

--- AppStoreApp/ViewControllers/ViewController.swift
+++ AppStoreApp/ViewControllers/ViewController.swift
@@ -11,9 +11,15 @@ class ViewController: UIViewController {
     let appViewController = AppDetailViewController()
     
     var allApps: [App]? = [
-        App(id: 1, trackName: "first", description: "first app's description", fileSizeBytes: 456789, formattedPrice: "$4.99", sellerName: "The Count", version: 1.0),
-        App(id: 2, trackName: "second", description: "second app's description", fileSizeBytes: 9876543, formattedPrice: "Free", sellerName: "Dracula", version: 1.1),
-        App(id: 3, trackName: "third", description: "third app's description", fileSizeBytes: 7655678, formattedPrice: "$1.99", sellerName: "Muahaha", version: 1.3)
+        App(id: 1, trackName: "first", artworkUrl100: "https://is5-ssl.mzstatic.com/image/thumb/Purple116/v4/a1/64/d5/a164d515-abef-fe20-da52-a1318242d3f6/source/100x100bb.jpg", description: "first app's description", screenshotUrls: [
+            "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource125/v4/2c/a4/bb/2ca4bbfc-0da3-f259-80aa-e3cd3cf6de75/94313b43-8a07-4be8-9eeb-548721eccbca_Apple_Biz_1.jpg/392x696bb.jpg",
+            "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource115/v4/bc/5a/1a/bc5a1ac1-6241-1a93-aa0c-fb4226794f5d/dfb909f9-84b6-4e81-85e0-666269a3cb5a_Apple_Biz_2.jpg/392x696bb.jpg"], fileSizeBytes: 456789, formattedPrice: "$4.99", sellerName: "The Count", version: "1.0"),
+        App(id: 2, trackName: "second", artworkUrl100: "https://is5-ssl.mzstatic.com/image/thumb/Purple116/v4/a1/64/d5/a164d515-abef-fe20-da52-a1318242d3f6/source/100x100bb.jpg", description: "second app's description", screenshotUrls: [
+            "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource125/v4/2c/a4/bb/2ca4bbfc-0da3-f259-80aa-e3cd3cf6de75/94313b43-8a07-4be8-9eeb-548721eccbca_Apple_Biz_1.jpg/392x696bb.jpg",
+            "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource115/v4/bc/5a/1a/bc5a1ac1-6241-1a93-aa0c-fb4226794f5d/dfb909f9-84b6-4e81-85e0-666269a3cb5a_Apple_Biz_2.jpg/392x696bb.jpg"], fileSizeBytes: 9876543, formattedPrice: "Free", sellerName: "Dracula", version: "1.1"),
+        App(id: 3, trackName: "third", artworkUrl100: "https://is5-ssl.mzstatic.com/image/thumb/Purple116/v4/a1/64/d5/a164d515-abef-fe20-da52-a1318242d3f6/source/100x100bb.jpg", description: "third app's description", screenshotUrls: [
+            "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource125/v4/2c/a4/bb/2ca4bbfc-0da3-f259-80aa-e3cd3cf6de75/94313b43-8a07-4be8-9eeb-548721eccbca_Apple_Biz_1.jpg/392x696bb.jpg",
+            "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource115/v4/bc/5a/1a/bc5a1ac1-6241-1a93-aa0c-fb4226794f5d/dfb909f9-84b6-4e81-85e0-666269a3cb5a_Apple_Biz_2.jpg/392x696bb.jpg"], fileSizeBytes: 7655678, formattedPrice: "$1.99", sellerName: "Muahaha", version: "1.3")
     ]
     var apps: [App]?
     var selectedIndex = 0
@@ -53,7 +59,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         title = "Search"
         self.navigationController?.navigationBar.isTranslucent = true
-        view.backgroundColor = .white
+        view.backgroundColor = .systemBackground
         
         // TODO: remove once you're pulling in data. Future improvement: use userdefaults to persist selection
         apps = allApps?.filter { $0.formattedPrice == "Free" }
