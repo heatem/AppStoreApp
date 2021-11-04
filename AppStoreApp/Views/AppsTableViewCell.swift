add3b8b4eb93a67935edeefac94c1f9e4c144d3c
Heather Mason <heatem@gmail.com>
2021-11-04 16:42:22 -0400

    expand placeholder app

--- AppStoreApp/Views/AppsTableViewCell.swift
+++ AppStoreApp/Views/AppsTableViewCell.swift
@@ -7,7 +7,22 @@
 
 import UIKit
 
-let app = App(id: 4, trackName: "Generic Result", description: "This is a generic placeholder app description. You should download my app. It's great.", fileSizeBytes: 81806336, formattedPrice: "Free", sellerName: "Heather", version: 1.0)
+let app = App(
+    id: 4,
+    trackName: "Generic Result",
+    artworkUrl100: "https://is5-ssl.mzstatic.com/image/thumb/Purple116/v4/a1/64/d5/a164d515-abef-fe20-da52-a1318242d3f6/source/100x100bb.jpg",
+    description: "This is a generic placeholder app description. You should download my app. It's great.",
+    screenshotUrls: [
+        "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource125/v4/2c/a4/bb/2ca4bbfc-0da3-f259-80aa-e3cd3cf6de75/94313b43-8a07-4be8-9eeb-548721eccbca_Apple_Biz_1.jpg/392x696bb.jpg",
+        "https://is1-ssl.mzstatic.com/image/thumb/PurpleSource115/v4/bc/5a/1a/bc5a1ac1-6241-1a93-aa0c-fb4226794f5d/dfb909f9-84b6-4e81-85e0-666269a3cb5a_Apple_Biz_2.jpg/392x696bb.jpg",
+        "https://is3-ssl.mzstatic.com/image/thumb/PurpleSource115/v4/12/f5/60/12f560f1-22a2-3580-241d-ea9bb92084cf/1b3d0841-90b7-463e-a973-d0697dfa5ebb_Apple_Biz_3.jpg/392x696bb.jpg",
+        "https://is2-ssl.mzstatic.com/image/thumb/PurpleSource115/v4/fe/a9/b7/fea9b78d-da0e-a533-d8b1-9cbdf6e12dfe/5b1ddee3-1b56-45ab-aa17-cca1c37b1063_Apple_Biz_4.jpg/392x696bb.jpg"
+    ],
+    fileSizeBytes: 81806336,
+    formattedPrice: "Free",
+    sellerName: "Heather",
+    version: "1.0"
+)
 
 class AppsTableViewCell: UITableViewCell {
     let appNameLabel: UILabel = {
